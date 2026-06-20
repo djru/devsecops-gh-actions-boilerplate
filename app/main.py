acb6e55ce8c0a7fafb6489a7d52083a0b4a7f987
Dan Ruswick <djruswick@outlook.com>
2026-06-20 16:20:18 -0400

    testing pipeline against known bad example

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 from starlette.applications import Starlette
-from starlette.responses import JSONResponse
+from starlette.responses import JSONResponse, Response
 from starlette.routing import Route
 import uvicorn
 
@@ -9,9 +9,18 @@ async def homepage(request):
 async def healthcheck(request):
     return JSONResponse({"status": "OK"})
 
+AWS_SECRET_ACCESS_KEY='wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY' # very bad
+
+async def unsafe_bad(request):
+    # DANGER: Directly evaluating raw user queries allows Remote Code Execution (RCE)
+    user_query = request.query_params.get("cmd", "")
+    result = eval(user_query) 
+    return Response(str(result))
+
 app = Starlette(routes=[
     Route('/', homepage),
-    Route('/health', healthcheck)
+    Route('/health', healthcheck),
+    Route('/bad', unsafe_bad)
 ])
 
 if __name__ == "__main__":
